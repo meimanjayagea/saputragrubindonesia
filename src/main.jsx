da4bf36cea2ca948ea1bc1812cb7bf0475b3f4ee
meiman_gea <meiman.gea@ist.id>
2022-05-21 14:24:46 +0700

    menambahkan templete login

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import Login from './containers/login'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Login/>
   </React.StrictMode>,
   document.getElementById('root')
 )
