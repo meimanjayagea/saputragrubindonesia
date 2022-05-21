dfdb0ddac9cc607be49a51b5727e5d90f9966a0a
meiman_gea <meiman.gea@ist.id>
2022-05-21 14:37:03 +0700

    update

--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
+import {Avatar, Button, CssBaseline, TextField, FormControlLabel} from '@material-ui/core';
+import {Checkbox, Link, Paper, Box, Grid, LockOutlinedIcon, Typography} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 function Copyright() {
   return (
+    <>
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://saputragrubindonesia.meiman17.repl.co/">
@@ -23,6 +14,7 @@ function Copyright() {
       {new Date().getFullYear()}
   
     </Typography>
+    </>
   );
 }
 
